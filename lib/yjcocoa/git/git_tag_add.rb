879576d3cba0d7aff42830a8b6ec6b290b9a6fd9
阳君 <937447974@qq.com>
2018-07-26 11:39:45 +0800

    perf: yjcocoa git tag add 增加分支区分

--- lib/yjcocoa/git/git_tag_add.rb
+++ lib/yjcocoa/git/git_tag_add.rb
@@ -16,20 +16,30 @@ module YJCocoa
         
         self.abstract_command = false
         self.command = 'add'
-        self.summary = '添加默认 tag(git库名-%Y%m%d%H%M)'
-        self.description = '添加默认 tag(git库名-%Y%m%d%H%M)'
+        self.summary = '添加默认 tag(git库名-branch-%Y%m%d%H%M)'
+        self.description = '添加默认 tag(git库名-branch-%Y%m%d%H%M)'
         
         def run
             if File.exist?(".git")
+                current_branch = self.current_branch
                 tag = File.basename(Dir.pwd)
-                tag << '-'
-                tag << Time.now.strftime("%Y%m%d%H%M")
+                tag << "-#{current_branch}" unless current_branch =~ /master/
+                tag << "-#{Time.now.strftime('%Y%m%d%H%M')}"
                 puts "YJCocoa build tag #{tag}".green
                 system("yjcocoa git tag --add=#{tag}")
-                else
+            else
                 Dir.chdir("..") {
                     self.run
                 }
+            end
+        end
+        
+        def current_branch
+            list = (`git branch`).split("\n")
+            for item in list
+                if item =~ /\* /
+                    return item.gsub(/\* /, "")
+                end
             end
         end
         
--- lib/yjcocoa/test.rb
+++ lib/yjcocoa/test.rb
@@ -1,1 +1,14 @@
-Dir.chdir("..") { system("yjcocoa git pull") }
+def current_branch
+    list = (`git branch`).split("\n")
+    for item in list
+        if item =~ /\* /
+            return item.gsub(/\* /, "")
+        end
+    end
+end
+
+unless current_branch =~ /master/
+    puts "current_branch1"
+end
+
+puts "current_branch"
